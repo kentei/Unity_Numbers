f4893611215664561cea59032b10da2ac9488fee
kentei_syunrai <become.cornerstone.of.my.hadou@gmail.com>
2016-08-17 21:30:07 +0900

    タイトル部分の表示実装(初回コミット)

---
+++ Assets/Scripts/GameStartButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStartButton : MonoBehaviour {
+
+    /// ボタンをクリックした時の処理
+    public void OnClick()
+    {
+        Debug.Log("Start Button!!");
+    }
+}
---
+++ Assets/Scripts/TitleMgr.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleMgr : MonoBehaviour {
+    private GUIStyle titleStyle;
+
+    void Start()
+    {
+        titleStyle = new GUIStyle();
+        titleStyle.fontSize = 30;
+    }
+
+    void OnGUI()
+    {
+        // フォントの位置
+        float w = 100; // 幅
+        float h = 30; // 高さ
+        float px = Screen.width / 2 - w / 2;
+        float py = Screen.height / 2 - h / 2;
+        titleStyle.fontSize = 30;
+        GUI.Label(new Rect(px, py - 50, w, h), "Numbers", titleStyle);
+        GUI.Button(new Rect(px, py, w, h), "ニューゲーム");
+        GUI.Button(new Rect(px, py + 50, w, h), "設定");
+    }
+}
